8e75a7112fe2f80d2e1ec58d3b75811b6a202858
axel cholet <axel.cholet@icloud.com>
2025-01-29 10:11:13 +0100

    les base du frontend

---
+++ assets/frontend.js
@@ -0,0 +1,9 @@
+// latitude 
+const nom_hotel = document.querySelector('#input_nom_hotel');
+const city_hotel = document.querySelector('#input_city_hotel');
+const adress_hotel = document.querySelector('#input_adress_hotel');
+const reference_hotel = document.querySelector('#input_reference_hotel');
+
+const ajout_bdd = document.querySelector('#button');
+
+console.log("pat patrouille");
