2fc740a84a15f7a3c8d6f2b6b3d51395feb040a7
axel cholet <axel.cholet@icloud.com>
2025-03-26 11:16:34 +0100

    sécurisation parti 2

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,5 +1,18 @@
 const authMiddleware = async (req, res, next) => {
+    try {
     const headers = req.headers;
-    console.log(headers);
+    const token = headers.authorization.split(' ')[1];
+
+    if (headers.authorization && jwt.verify(headers.authorization, 'azertyuiop')) 
+    {
+        return next();
+    }
+
+
+    }catch (error) {
+    return res.status(403).json({ 
+        message: "Unauthorized" 
+    });
+}
 }
 export default authMiddleware;
